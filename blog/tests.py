455042aa6ea6b9b0fa1f2f547bfd2021c03ad986
btuuganbekov <btuuganbekov@megacom.kg>
2022-08-25 14:58:07 +0600

    add github actions

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Post
+
 
 # Create your tests here.
+class PostTesting(TestCase):
+
+    def setUp(self) -> None:
+        self.blog = Post.objects.create(
+            title='django',
+            author='Sam',
+            slug='django slug'
+        )
+
+    def test_post_model(self):
+        d = self.blog
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d), 'django')
